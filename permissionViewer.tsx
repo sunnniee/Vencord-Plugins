3a52747e21abb5d0a54652c4b2816ab67038e3ef
ActuallyTheSun <78964224+ActuallyTheSun@users.noreply.github.com>
2022-12-01 14:36:25 +0200

    PermissionViewer: visual improvements

--- permissionViewer.tsx
+++ permissionViewer.tsx
@@ -16,31 +16,42 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+import { LazyComponent } from "@utils/misc.jsx";
+import { ModalContent, ModalHeader, ModalRoot, ModalSize, openModal } from "@utils/modal.jsx";
+import { Queue } from "@utils/Queue";
+import definePlugin from "@utils/types";
+import { findByCode, findByPropsLazy, waitFor } from "@webpack";
+import { ChannelStore, GuildStore, Menu, React, Text, Tooltip, UserStore, UserUtils } from "@webpack/common";
 import { Channel, Guild, User } from "discord-types/general";
 
-import { ModalContent, ModalHeader, ModalRoot, ModalSize, openModal } from "../utils/modal.jsx";
-import definePlugin from "../utils/types";
-import { ChannelStore, GuildStore, Menu, React, Text } from "../webpack/common.jsx";
-import { waitFor } from "../webpack/webpack.js";
 
 let Permissions: Record<string, bigint>, computePermissions: ({ ...args }) => bigint;
 waitFor(["VIEW_CREATOR_MONETIZATION_ANALYTICS"], m => Permissions = m);
 waitFor(["canEveryoneRole"], m => ({ computePermissions } = m));
+const UserSummaryItem = LazyComponent(() => findByCode("defaultRenderUser", "showDefaultAvatarsForNullUsers"));
+const OwnerCrown = LazyComponent(() => findByCode("M13.6572 5.42868C13.8879 5.29002"));
+const OwnerCrownStyles = findByPropsLazy("ownerIcon", "avatarDecorationPadding");
 
 function toTitleCase(str: string): string {
     return str.split("_")
         .map(seg => seg.charAt(0) + seg.slice(1).toLowerCase()).join(" ");
 }
 
-function getPermissions(user: User, channel: Channel) {
+const ownerFetchQueue = new Queue();
+
+function getPermissions(user: User, channel: Channel, guild?: Guild) {
     if (!channel) return 0n;
-    const guild = GuildStore.getGuild(channel.guild_id);
+    guild ??= GuildStore.getGuild(channel.guild_id);
     if (!guild) return 0n;
     return computePermissions({ user, context: guild, overwrites: channel.permissionOverwrites });
 }
 
 interface PermissonsProps {
     permissions: bigint;
+    [key: string]: any;
+}
+interface PermIconProps {
+    variant: "allowed" | "denied";
     [key: string]: any;
 }
 interface GuildMenuProps {
@@ -55,25 +66,61 @@ function makeRoleDot(color: number) {
 
 // this plugin is proof i shouldn't be allowed anywhere near react
 const Components = {
-    permission: {
-        allowed: () => <svg height="36" viewBox="-4 -4 44 44" width="36"><path d="M0 0h36v36H0z" fill="none" /><path fill="#3ba55d" d="M13.5 24.255 7.245 18l-2.13 2.115L13.5 28.5 31.5 10.5l-2.115 -2.115z" /></svg>,
-        denied: () => <svg height="36" viewBox="-4 -4 56 56" width="36"><path d="M0 0h48v48H0z" fill="none" /><path fill="#ed4245" d="M24 4C12.96 4 4 12.96 4 24s8.96 20 20 20 20-8.96 20-20S35.04 4 24 4zm0 36c-8.84 0-16-7.16-16-16 0-3.7 1.26-7.1 3.38-9.8L33.8 36.62C31.1 38.74 27.7 40 24 40zm12.62-6.2L14.2 11.38C16.9 9.26 20.3 8 24 8c8.84 0 16 7.16 16 16 0 3.7-1.26 7.1-3.38 9.8z" /></svg>
+    PermissionIcon(props: PermIconProps) {
+        switch (props.variant) {
+            case "allowed":
+                return <svg height="36" viewBox="-4 -4 44 44" width="36"><path d="M0 0h36v36H0z" fill="none" /><path fill="#3ba55d" d="M13.5 24.255 7.245 18l-2.13 2.115L13.5 28.5 31.5 10.5l-2.115 -2.115z" /></svg>;
+            case "denied":
+            default: // will never happen but ts yells at me if i dont include it
+                return <svg height="36" viewBox="-4 -4 56 56" width="36"><path d="M0 0h48v48H0z" fill="none" /><path fill="#ed4245" d="M24 4C12.96 4 4 12.96 4 24s8.96 20 20 20 20-8.96 20-20S35.04 4 24 4zm0 36c-8.84 0-16-7.16-16-16 0-3.7 1.26-7.1 3.38-9.8L33.8 36.62C31.1 38.74 27.7 40 24 40zm12.62-6.2L14.2 11.38C16.9 9.26 20.3 8 24 8c8.84 0 16 7.16 16 16 0 3.7-1.26 7.1-3.38 9.8z" /></svg>;
+        }
+
     },
-    separator: () => <div style={{ boxSizing: "border-box", margin: "2px", borderBottom: "1px solid var(--background-modifier-accent)" }} />,
-    padding: () => <div style={{ padding: "8px" }} />,
-    permissions: (props: PermissonsProps) => <>
+    Separator: () => <div style={{ boxSizing: "border-box", margin: "2px", borderBottom: "1px solid var(--background-modifier-accent)" }} />,
+    Padding: () => <div style={{ padding: "8px" }} />,
+    UserAvatar: ({ user, guildId }: { user: User, guildId: string; }) =>
+        <UserSummaryItem users={[user]} guildId={guildId} showDefaultAvatarsForNullUsers showUserPopout />,
+    OwnerIcon: () => <Tooltip text={"Server Owner"}>
+        {tooltipProps => <OwnerCrown className={OwnerCrownStyles.ownerIcon} {...tooltipProps} />}
+    </Tooltip>,
+    OwnerInfo: ({ guild }: { guild: Guild; }) => {
+        const [ownerUser, setOwnerUser] = React.useState(UserStore.getUser(guild.ownerId) || null);
+        if (!ownerUser) ownerFetchQueue.push(() => UserUtils.fetchUser(guild.ownerId)
+            .then(user => setOwnerUser(user))
+        );
+        return <div style={{
+            backgroundColor: "var(--background-secondary)",
+            borderRadius: 8,
+            padding: 2,
+            margin: "0 4px",
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 4
+        }}>
+            <Components.OwnerIcon />
+            <Components.UserAvatar user={ownerUser} guildId={guild.id} />
+            {ownerUser
+                ? <Text variant="text-md/bold" tag="span">{ownerUser.username}
+                    <Text variant="text-md/bold" tag="span" color="text-muted">#{ownerUser.discriminator}</Text>
+                </Text>
+                : <Text variant="text-md/bold" tag="span">Loading</Text>
+            }
+        </div>;
+    },
+    Permissions: (props: PermissonsProps) => <>
         {Object.entries(Permissions).map(([perm, permInt]) =>
             <>
                 <Text variant="text-md/semibold">
-                    {props.permissions & permInt ? <Components.permission.allowed /> : <Components.permission.denied />}
+                    <Components.PermissionIcon variant={props.permissions & permInt ? "allowed" : "denied"} />
                     {toTitleCase(perm)}
                 </Text>
-                <Components.separator />
+                <Components.Separator />
             </>
         )}
-        <Components.padding />
+        <Components.Padding />
     </>,
-    guildPermissions(props: GuildMenuProps) {
+    GuildPermissions(props: GuildMenuProps) {
         const { guild } = props;
         const roles = Object.entries(guild.roles);
         const [role, setRole] = React.useState(roles[0][1].id);
@@ -95,7 +142,7 @@ const Components = {
             </div>
             <div>
                 <Text variant="text-lg/semibold">Permissions for {guild.roles[role].name}</Text>
-                <Components.permissions permissions={guild.roles[role].permissions} />
+                <Components.Permissions permissions={guild.roles[role].permissions} />
             </div>
         </div>;
     }
@@ -112,14 +159,14 @@ export default definePlugin({
     patches: [{
         find: ".GUILD_CHANNEL_USER_MENU])",
         replacement: {
-            match: /\.Fragment,{children:\[(.{1,150})\[(.{20,80})\]/,
-            replace: ".Fragment,{children:[$1[$2,Vencord.Plugins.plugins.PermissionViewer.userMenuItem(arguments[0])]"
+            match: /\.Fragment,{children:\[.{1,150}\[.{20,80}\]/,
+            replace: "$&.concat([Vencord.Plugins.plugins.PermissionViewer.userMenuItem(arguments[0])])"
         }
     }, {
         find: "GuildContextMenu: user",
         replacement: {
-            match: /children:\[(.{1,2}),__OVERLAY__(.{1,20})\]/,
-            replace: "children:[$1,__OVERLAY__$2,Vencord.Plugins.plugins.PermissionViewer.guildMenuItem(arguments[0].guild)]"
+            match: /children:\[.{1,2},__OVERLAY__.{1,20}\]/,
+            replace: "$&.concat([Vencord.Plugins.plugins.PermissionViewer.guildMenuItem(arguments[0].guild)])"
         }
     }],
 
@@ -127,19 +174,26 @@ export default definePlugin({
         const { channelId, user } = props;
         const channel = ChannelStore.getChannel(channelId);
         if (!channel) return null;
+        const guild = GuildStore.getGuild(channel.guild_id);
+        if (!guild) return null;
         return <Menu.MenuItem
             id="permissions"
             key="permissions"
             label="View Permissions"
             action={() => {
-                const perms = getPermissions(user, channel);
+                const perms = getPermissions(user, channel, guild);
                 openModal(modalProps =>
                     <ModalRoot size={ModalSize.SMALL} {...modalProps}>
                         <ModalHeader>
-                            <Text variant="heading-md/bold">{user.username}#{user.discriminator}</Text>
+                            <Components.UserAvatar user={user as User} guildId={channel.guild_id} />
+                            <Text variant="heading-md/bold" style={{ paddingLeft: 4 }}>
+                                {user.username}
+                                <Text variant="heading-md/bold" color="text-muted" tag="span">#{user.discriminator}</Text>
+                            </Text>
+                            {guild.ownerId === user.id && <Components.OwnerIcon />}
                         </ModalHeader>
                         <ModalContent>
-                            <Components.permissions permissions={perms} />
+                            <Components.Permissions permissions={perms} />
                         </ModalContent>
                     </ModalRoot>
                 );
@@ -157,10 +211,17 @@ export default definePlugin({
                 openModal(modalProps =>
                     <ModalRoot size={ModalSize.LARGE} {...modalProps}>
                         <ModalHeader>
-                            <Text variant="heading-md/bold">{guild.name}</Text>
+                            <img
+                                src={`https://${window.GLOBAL_ENV.CDN_HOST}/icons/${guild.id}/${guild.icon}.png?size=32`}
+                                width={32}
+                                height={32}
+                                style={{ borderRadius: "50%" }}
+                            />
+                            <Text variant="heading-md/bold" style={{ paddingLeft: 4 }}>{guild.name}</Text>
+                            <Components.OwnerInfo guild={guild} />
                         </ModalHeader>
                         <ModalContent>
-                            <Components.guildPermissions guild={guild} />
+                            <Components.GuildPermissions guild={guild} />
                         </ModalContent>
                     </ModalRoot>
                 );
